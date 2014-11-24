503013d07c30be206e15d406d7d5f5fbdc76a494
Amit <Amit@Amits-MacBook-Pro.local>
2014-11-25 00:35:54 +0530

    Initial Implementation

--- iCarouselDemo/ViewController.m
+++ iCarouselDemo/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "iCarousel.h"
 
-@interface ViewController ()
+@interface ViewController () <iCarouselDataSource,iCarouselDelegate>
 
+@property (weak, nonatomic) IBOutlet iCarousel *iCarouselV;
 @end
 
 @implementation ViewController
@@ -18,12 +20,67 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //self.iCarouselV.wrapEnabled = YES;
+    self.iCarouselV.type = iCarouselTypeLinear;
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - iCarouselDataSource methods
+- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return 4;
 }
 
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
+{
+    static NSInteger lastIdx = -1;
+    static NSInteger lastVal =0;
+    
+    if (lastIdx != -1) {
+        
+        NSInteger diff = index - lastIdx;
+        
+        if (diff == 1) {
+            lastVal++;
+        }else if(diff == -1) {
+            lastVal--;
+        }else if(diff > 0) {
+            lastVal --;
+        }else if(diff < 0) {
+            lastVal++;
+        }
+    }
+    
+    UILabel *lbl = (UILabel *)view;
+    if (!lbl) {
+        NSLog(@"creating view for index %d",index);
+        CGRect frm = CGRectMake(0, 0, carousel.frame.size.width, 20);
+        frm.origin = carousel.center;
+        lbl = [[UILabel alloc] initWithFrame:frm];
+        lbl.textAlignment = NSTextAlignmentCenter;
+        lbl.backgroundColor = [UIColor whiteColor];
+    }
+    NSLog(@"%d",index);
+    lbl.text = [NSString stringWithFormat:@"%d",lastVal];
+    lbl.backgroundColor = (index%2)?[UIColor whiteColor]:[UIColor purpleColor];
+    
+    
+    lastIdx = index;
+    return lbl;
+}
+
+#pragma mark - iCarouselDelegate methods
+- (CGFloat)carousel:(iCarousel *)carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value
+{
+    CGFloat val = value;
+    switch (option) {
+        case iCarouselOptionWrap:
+            val = 1;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return val;
+}
 @end
