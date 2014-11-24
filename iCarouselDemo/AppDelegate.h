b590305994134f9db145924df517f2395f7e7b00
Amit <Amit@Amits-MacBook-Pro.local>
2014-11-24 23:25:24 +0530

    Initial Commit

---
+++ iCarouselDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iCarouselDemo
+//
+//  Created by Amit Jain on 24/11/14.
+//  Copyright (c) 2014 ajonnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iCarouselDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iCarouselDemo
+//
+//  Created by Amit Jain on 24/11/14.
+//  Copyright (c) 2014 ajonnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
